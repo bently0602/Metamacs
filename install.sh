7c2ab5a9528044ccb810d3de8469ec557c9dae09
bently0602 <atx.hx.ag@gmail.com>
2021-01-28 18:49:34 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo 'emacsclient --create-frame --alternate-editor=""' >> /bin/metamacs
 chmod +x /bin/metamacs
 
 rm -f ~/.emacs.d/init.el
-curl -o ~/.emacs.d/init.el https://github.com/bently0602/Metamacs/init.el
+curl -o ~/.emacs.d/init.el https://raw.githubusercontent.com/bently0602/Metamacs/main/init.el
 
 echo 'Done!'
 echo 'metamacs is the new command'
